393b584599eb34e0f2ba6fe444b287a6d7b5a6ec
Romulo P T Mota <romuloptmota@gmail.com>
2023-02-11 13:26:58 -0300

    create templates

--- login_autentication/settings.py
+++ login_autentication/settings.py
@@ -131,3 +131,6 @@ AUTH_USER_MODEL = 'core.CustomUsuario'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'

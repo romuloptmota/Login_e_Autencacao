ff5af5940361efe4f740a1393364b48f9ba67ad8
Romulo P T Mota <romuloptmota@gmail.com>
2023-02-11 11:45:31 -0300

    create models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UsuarioManager(BaseUserManager):
+
+    use_in_migrations = True  # authentication migration
+
+    # create user common or supersuser
+    def _create_user(self, email, password, **extra_field):
+        if not email:
+            raise ValueError('O e-mail é obrigatório')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # permission user common
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    # permission superuser
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser precisa ter is_superuser=True')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser precisa ter is_staff=True')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUsuario(AbstractUser):
+    email = models.EmailField('E-mail', unique=True)
+    fone = models.CharField('Telefone', max_length=15)
+    is_staff = models.BooleanField('Membro da equipe', default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'fone']
+
+    def __str__(self):
+        return self.email
+
+    objects = UsuarioManager()
